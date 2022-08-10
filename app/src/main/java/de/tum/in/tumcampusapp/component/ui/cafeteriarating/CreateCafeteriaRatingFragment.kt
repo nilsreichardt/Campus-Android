0101b9d05a3f9bda1d3d92a52fdd316801c544ec
Tobias Jungmann <tobias.jungmann@tum.de>
2022-08-10 10:50:12 +0200

    added prove of concept camera

--- app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteriarating/CreateCafeteriaRatingFragment.kt
+++ app/src/main/java/de/tum/in/tumcampusapp/component/ui/cafeteriarating/CreateCafeteriaRatingFragment.kt
@@ -1,15 +1,32 @@
 package de.tum.`in`.tumcampusapp.component.ui.cafeteriarating
 
 
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import de.tum.`in`.tumcampusapp.R
 import de.tum.`in`.tumcampusapp.component.other.generic.fragment.FragmentForAccessingTumCabe
+import de.tum.`in`.tumcampusapp.component.tumui.feedback.FeedbackContract
+import de.tum.`in`.tumcampusapp.component.tumui.feedback.FeedbackPresenter
 import de.tum.`in`.tumcampusapp.databinding.FragmentCafeteriaRatingBinding
+import de.tum.`in`.tumcampusapp.utils.ImageUtils
+import de.tum.`in`.tumcampusapp.utils.Utils
+import java.io.File
+import java.io.IOException
+import javax.inject.Inject
 
 
 class CreateCafeteriaRatingFragment : FragmentForAccessingTumCabe<List<String>>(
@@ -17,6 +34,11 @@ class CreateCafeteriaRatingFragment : FragmentForAccessingTumCabe<List<String>>(
     R.string.create_cafeteria_rating
 ), AdapterView.OnItemSelectedListener {
 
+
+    // used to access the photo functionality
+    // todo potentially split to another camera "forwarder"
+    //  @Inject
+    //  lateinit var presenter: FeedbackContract.Presenter
 
     private val binding by viewBinding(FragmentCafeteriaRatingBinding::bind)
 
@@ -53,7 +75,14 @@ class CreateCafeteriaRatingFragment : FragmentForAccessingTumCabe<List<String>>(
         binding.tagOptionListRecyclerView.layoutManager = layoutManager
         binding.tagOptionListRecyclerView.adapter = createTagRatingAdapter
 
+// todo eigeneer contract und  co benötigt?
+        /*presenter.attachView(view)
 
+        if (savedInstanceState != null) {
+            presenter.onRestoreInstanceState(savedInstanceState)
+        }*/
+
+        binding.addImageButton.setOnClickListener { showImageOptionsDialog() }
     }
 
     private fun prepareItems() {
@@ -61,12 +90,99 @@ class CreateCafeteriaRatingFragment : FragmentForAccessingTumCabe<List<String>>(
         itemsList.add(CreateTagRatingElement("Variety General"))
         itemsList.add(CreateTagRatingElement("Variety Vegetarian"))
         itemsList.add(CreateTagRatingElement("Variety Vegan"))
-        itemsList.add (CreateTagRatingElement("Enough free tables"))
+        itemsList.add(CreateTagRatingElement("Enough free tables"))
+    }
 
 
+    private var currentPhotoPath: String? = null
 
 
-        //createTagRatingAdapter.notifyDataSetChanged()
+    private fun showImageOptionsDialog() {
+        val options =
+            arrayOf(getString(R.string.feedback_take_picture), getString(R.string.gallery))
+        val alertDialog = AlertDialog.Builder(requireContext())
+            .setTitle(R.string.feedback_add_picture)
+            .setItems(options) { _, index -> onImageOptionSelected(index) }
+            .setNegativeButton(R.string.cancel, null)
+            .create()
+        alertDialog.window?.setBackgroundDrawableResource(R.drawable.rounded_corners_background)
+        alertDialog.show()
+    }
+
+    fun onImageOptionSelected(option: Int) {
+        if (option == 0) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || checkPermission(Manifest.permission.CAMERA)) {
+                takePicture()
+            }
+        } else {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                openGallery()
+            }
+        }
+    }
+
+    /**
+     * @return true if user has given permission before
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private fun checkPermission(permission: String): Boolean {
+        val permissionCheck = ContextCompat.checkSelfPermission(requireContext(), permission)
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            val requestCode = when (permission) {
+                Manifest.permission.READ_EXTERNAL_STORAGE -> FeedbackPresenter.PERMISSION_FILES
+                Manifest.permission.CAMERA -> FeedbackPresenter.PERMISSION_CAMERA
+                else -> FeedbackPresenter.PERMISSION_LOCATION
+            }
+
+            showPermissionRequestDialog(permission, requestCode)
+            return false
+        }
+        return true
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    fun showPermissionRequestDialog(permission: String, requestCode: Int) {
+        requestPermissions(arrayOf(permission), requestCode)
+    }
+
+    fun takePicture() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        // Create the file where the photo should go
+        var photoFile: File? = null
+        try {
+            photoFile = ImageUtils.createImageFile(requireContext())
+            currentPhotoPath = photoFile.absolutePath
+        } catch (e: IOException) {
+            Utils.log(e)
+        }
+
+        if (photoFile == null) {
+            return
+        }
+
+        val authority = "de.tum.in.tumcampusapp.fileprovider"
+        val photoURI = FileProvider.getUriForFile(requireContext(), authority, photoFile)
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        try {
+            openCamera(takePictureIntent)
+        } catch (e: ActivityNotFoundException) {
+            photoFile.delete()
+        }
+    }
+
+    fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+       // val chooser = Intent.createChooser(intent, "Select file")
+      //  openGallery(chooser)
+    }
+
+    fun openCamera(intent: Intent) {
+        startActivityForResult(intent, FeedbackPresenter.REQUEST_TAKE_PHOTO)
     }
     //  val cafeterias = arrayOf("mensa garching", "mensa leopoldstrasse")
 
